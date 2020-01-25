f330ff529157042dcd5be832ac2fd555487e9047
yang821 <yang821@sina.com>
2020-01-25 18:19:49 +0800

    Add some UT.

--- Algorithm/src/v/algorithm/sort/Insert.java
+++ Algorithm/src/v/algorithm/sort/Insert.java
@@ -2,6 +2,13 @@ package v.algorithm.sort;
 
 import java.util.Arrays;
 
+/**
+ * @author v
+ * 
+ *         git clone https://github.com/yang821/practice.git
+ * 
+ *         git push -u origin master
+ */
 public class Insert {
 	public static void sort(int[] a) {
 		if (a == null || a.length < 2) {
@@ -22,9 +29,26 @@ public class Insert {
 	}
 
 	public static void main(String[] args) {
+		test1();
+		test2();
+		test3();
+	}
+
+	public static void test1() {
 		int[] array = new int[] { 3, 2, 1, 5, 7, 6, 9 };
 		sort(array);
+		System.out.println(Arrays.toString(array));
+	}
 
+	public static void test2() {
+		int[] array = new int[] { 3 };
+		sort(array);
+		System.out.println(Arrays.toString(array));
+	}
+
+	public static void test3() {
+		int[] array = new int[] {};
+		sort(array);
 		System.out.println(Arrays.toString(array));
 	}
 }
